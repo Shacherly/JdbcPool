cf5660e3ea9117850dd8a0a80e5eb9f478e7f08e
ast.Mufasa@Shachely <shizehao.vip@gmail.com>
2020-04-16 15:57:46 +0800

    '测试commit'

---
+++ src/test/stack/TestStack.java
@@ -0,0 +1,13 @@
+package test.stack;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+public class TestStack {
+
+    @Test
+    public void test1() {
+        Stack<String> st = new Stack<>();
+    }
+}
